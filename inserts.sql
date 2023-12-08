2bb7e1cd1d18988e52da22456a9848385136d465
MonPse <148347044+MonPse@users.noreply.github.com>
2023-12-08 14:29:21 +0100

    Update inserts.sql
    Yo je commit

--- inserts.sql
+++ inserts.sql
@@ -64,7 +64,24 @@ create table LOCATIONS (
 
 commit ;
 
-insert into FILM values (1, 'Salut les copines', 'Valérie Damidot', 'Valérie Damidot', 0, 'Drame', 10, true)
-insert into FILM values (2, 'Disparition au Habbo Hotel', 'Michel Houellebecq', 'Sean Connery', 0, 'Polar', 13, true)
-insert into FILM values (3, 'Renaissance Park', 'Quentin Tarantino', 'Léna Situations', 0, 'Film historique', 0, true)
+insert into FILM values (1, 'Salut les copines', 'Valerie Damidot', 'Valerie Damidot', 0, 'Drame', 10, true);
+insert into FILM values (2, 'Disparition au Habbo Hotel', 'Michel Houellebecq', 'Sean Connery', 0, 'Polar', 13, true);
+insert into FILM values (3, 'Renaissance Park', 'Quentin Tarantino', 'Lena Situations', 0, 'Film historique', 0, true);
+insert into FILM values (4, 'Douchebag', 'Emmanuel Micron', 'Karine Leclerc', 0, 'Dystopie', 18 , true);
+insert into FILM values (5, "La congolexicomatisation des especes endemiques de l'ile d'Ouessant", 'Commandant Cousteau', 'Habib Dunant', 0, 'Reportage', 4, true);
+insert into FILM values (6, 'Douche froide sous les tropiques', 'Ange Doume', 'Quentin Tarantino', 0, 'Drame', 12, true);
+insert into FILM values (7, 'Renaissance Park 2', 'Quentine Tarantino', 'Lena Situations', 0, 'Film historique', 18, true);
+insert into FILM values (8, 'Dormir dans la soupe', 'Louis de Funes', 'Dany Brillant', 0, 'Comedie', 10, true);
+insert into FILM values (9, "Soulager les douleurs hemorroidaires grace a l'injection de jus de concombre", 'Michel Cymes', 'Valerie Damidot', 0, 'Reportage', 63, true);
+insert into FILM values (10, 'Lent et tout mims avec du miel et des tricycles', 'Le Caillou', 'Sylvain Vilebrequin', 0, 'Course automobile', 14, true);
 
+insert into ABONNE values (1, 'Denis', 'Grognard', 1995-10-26, '1 Rue de la Poste, 59320 Hallennes Lez Haubourdin', 29, false)
+insert into ABONNE values (2, 'Pascale', 'Legitime' 1845-02-07, '256 Avenue Fochent, 38160 Chatte', 42, false);
+insert into ABONNE values (3, 'Damidot', 'Valerie' 2002-06-11, '9 Impasse de Monavenir, 23600 Saint-Silvain Bas le Roc', 5, false);
+insert into ABONNE values (4, 'Corinne', 'David-Souris', 2010-07-23, "44 Chemin d'Oustres, 58470 Saincaize Meauce", 88, false);
+insert into ABONNE values (5, 'Travis', 'Scout', 1473-12-30, '5841 Chemin de Chelieu, 27680 Vieux Port', 13, false);
+insert into ABONNE values (6, 'Frabice', 'Jaounot', 1966-08-07, "624 Rue de l'Oracle, 62142 Bellebrune", 9999, false);
+insert into ABONNE values (7, 'Herve', 'Djomb', 1975-02-14, '458 Passage sous les arbres, 85320 Les Pineaux', 124, false);
+insert into ABONNE values (8, 'Monique', 'Vlan', 2004-11-12, '74 Avenue Dranche, 93170 Bagnolet', 7, false);
+insert into ABONNE values (9, 'Gabine', 'Tronche', 1582-09-30, "4 Privet Drive, Placard sous l'escalier, 72350 Brulon", 16, false);
+insert into ABONNE values (10, 'Francis', 'Cabri', 1953-11-22, '8 Rue des Soupirs, 47000 Agen', 35, false);
