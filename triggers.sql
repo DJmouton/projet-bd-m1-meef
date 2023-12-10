f932945e7eb3d0bff545e55d5eb1517c12ace493
DJmouton <themouton26@gmail.com>
2023-12-10 18:27:28 +0100

    Ajustage des mot clés dans triggers

--- triggers.sql
+++ triggers.sql
@@ -1,12 +1,12 @@
 CREATE TRIGGER max_nb_blueray
 BEFORE INSERT
-ON Support FOR EACH ROW
+ON SUPPORT FOR EACH ROW
 DECLARE
     nb_blueray NUMBER
 BEGIN
     SELECT Count(*) in nb_blueray
-    FROM Support
-    WHERE support = "blueray"
+    FROM SUPPORT
+    WHERE type_supp = "blueray"
     IF (nb_blueray > 100) THEN
         RAISE_APPLICATION_ERROR("Plus de 100 Bluerays dans la machine");
     END IF;
@@ -14,10 +14,10 @@ END
 
 CREATE TRIGGER minimum_montant_carte
 BEFORE INSERT OR UPDATE
-ON Abonne FOR EACH ROW
+ON ABONNE FOR EACH ROW
 BEGIN
     SELECT montant
-    FROM Abonne
+    FROM ABONNE
     IF NEW.montant < 15 THEN 
         RAISE_APPLICATION_ERROR("Minimum 15€ nécessaire sur le compte");
     END IF;
@@ -25,10 +25,10 @@ END
 
 CREATE TRIGGER minimum_credit
 BEFORE UPDATE
-ON Abonne FOR EACH ROW
+ON ABONNE FOR EACH ROW
 BEGIN
     SELECT montant
-    FROM Abonne
+    FROM ABONNE
     IF (NEW.montant - montant > 0 AND NEW.montant - montant < 10) THEN 
         RAISE_APPLICATION_ERROR("Montant crédité inférieur à 10€");
     END IF;
@@ -36,12 +36,12 @@ END
 
 CREATE TRIGGER location_total_avec_abonnement
 BEFORE INSERT
-ON Locations FOR EACH ROW
+ON LOCATIONS FOR EACH ROW
 DECLARE
     nb_locations NUMBER
 BEGIN
     SELECT Count(*) IN nb_locations
-    FROM Locations JOIN Abonne USING (id_client)
+    FROM LOCATIONS JOIN ABONNE USING (id_client)
     GROUP BY id_client
     WHERE support = NULL
     IF (nb_locations > 3) THEN
@@ -51,15 +51,15 @@ END
 
 CREATE TRIGGER location_total_sans_abonnement
 BEFORE INSERT
-ON Locations FOR EACH ROW
+ON LOCATIONS FOR EACH ROW
 DECLARE
     nb_locations NUMBER
 BEGIN
     SELECT Count(*) IN nb_locations
-    FROM Locations
+    FROM LOCATIONS
     EXCLUDE
         SELECT id_client
-        FROM Abonne
+        FROM ABONNE
     GROUP BY id_client
     WHERE support = NULL
     IF (nb_locations > 1) THEN
@@ -69,16 +69,16 @@ END
 
 CREATE TRIGGER gel
 AFTER UPDATE
-ON Abonne FOR EACH ROW
+ON ABONNE FOR EACH ROW
 BEGIN
     SELECT montant
-    FROM Abonne
+    FROM ABONNEe
     IF (NEW.montant < 0) THEN
-        UPDATE Abonne
+        UPDATE ABONNE
         SET gele = true;
     END IF;
     IF (NEW.montant > 15) THEN
-        UPDATE Abonne
+        UPDATE ABONNE
         SET gele = false;
     END IF;
 END
